994b3332a0ee4267c08a6db412fff76aeff0b44d
Vladislav Grubov <orangechaton@gmail.com>
2022-12-09 13:08:52 +0300

    etcd fencing: implements double watch to ensure that instance will not stepdown in less than fencing_timeout

--- test/app/conf.lua
+++ test/app/conf.lua
@@ -1,5 +1,5 @@
 etcd = {
-	instance_name = 'instance_01',
+	instance_name = os.getenv("TT_INSTANCE_NAME"),
 	prefix = '/instance',
 	endpoints = {"http://etcd:2379"},
 	fencing_enabled = true,
@@ -8,7 +8,10 @@ etcd = {
 box = {
 	background = false,
 	log_level = 6,
-	log_format = 'plain'
+	log_format = 'plain',
+
+	memtx_dir = '/var/lib/tarantool/snaps/',
+	wal_dir = '/var/lib/tarantool/xlogs',
 }
 
 app = {
